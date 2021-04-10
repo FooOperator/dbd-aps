6f743635bd72eb317161b45633a047b7d172540c
Lucas Guerra Cardoso <devgcard@gmail.com>
2021-04-10 14:26:34 -0300

    métodos get, post, createConnection e delete funcionando, falta put e entriesSort.

--- src/Database.java
+++ src/Database.java
@@ -9,8 +9,22 @@ public class Database {
 
     public static void main(String args[]) {
         Database kj = new Database();
+        User test1 = new User("Lucas Guerra Cardoso",
+                "Masculino",
+                (byte) 21,
+                "example@gmail.com",
+                "Ciência Da Computação",
+                "11122233345",
+                "Alunos");
         User test2;
-
+        try {
+            kj.createConnection();
+            kj.post(test1);
+            test2 = kj.get("Alunos", "Lucas Guerra Cardoso");
+            System.out.println(test1.getName());
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
     public void createConnection() throws ClassNotFoundException, SQLException {
         String dbURL = "jdbc:mysql://127.0.0.1:3302/dbd";
@@ -19,7 +33,7 @@ public class Database {
         try {
             Connection conn = DriverManager.getConnection(dbURL, username, password);
             db = conn;
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             System.out.println("Connection Established!");
@@ -27,16 +41,16 @@ public class Database {
         
     }
     public void post(User entry) throws SQLException {
-        String sql = String.format("INSERT INTO %s (nome, idade, email, curso, cpf) VALUES (?, ?, ?, ?, ?)", entry.getTable());
+        String sql = String.format("INSERT INTO %s (nome, idade, email, curso, cpf, genero) VALUES (?, ?, ?, ?, ?, ?)", entry.getTable());
         
         try {
             PreparedStatement ps = db.prepareStatement(sql);
-            ps.setString(1, entry.getTable());
             ps.setString(1, entry.getName());
             ps.setByte(2, entry.getAge());
             ps.setString(3, entry.getEmail());
             ps.setString(4, entry.getCourse());
             ps.setString(5, entry.getCpf());
+            ps.setString(6, entry.getGender());
             ps.execute();
         } catch (Exception e) {
             e.printStackTrace();
@@ -52,7 +66,7 @@ public class Database {
         User user = new User(" ", "", (byte)0, " ", " ", " ", " ");
         try {
             while (rs.next()) {
-                user = new User(rs.getString("nome"), rs.getString(""), rs.getByte("idade"), rs.getString("email"), rs.getString("curso"), rs.getString("cpf"), table);
+                user = new User(rs.getString("nome"), rs.getString("genero"), rs.getByte("idade"), rs.getString("email"), rs.getString("curso"), rs.getString("cpf"), table);
             } 
             return user;
         } catch (SQLException e) {
@@ -78,7 +92,9 @@ public class Database {
         }
         
     }
-    public void entries(){
-
+    public void entriesSort(String table, String gender) throws SQLException {
+        String sql = String.format("SELECT * FROM %s WHERE genero LIKE ?", table);
+        PreparedStatement ps = db.prepareStatement(sql);
+        ps.setString(1, gender);
     }
 }
