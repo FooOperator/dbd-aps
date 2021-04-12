8712fa309fd226f7bc26112168ed76afc16f836d
Lucas Guerra Cardoso <devgcard@gmail.com>
2021-04-11 21:02:52 -0300

    query agora reflete a tabela atual

--- sql/CREATE TABLE ALUNOS.sql
+++ sql/CREATE TABLE ALUNOS.sql
@@ -4,6 +4,7 @@ CREATE TABLE Alunos (
     idade smallint not null,
     cpf numeric(11) not null unique,
     email varchar(320) not null unique,
+    curso varchar(40) not null,
     genero varchar(10),
 	
     primary key(cpf)
